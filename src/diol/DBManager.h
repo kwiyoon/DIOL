a1a307672df9b38f1704113767e9a2e19e3ec426
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-05-25 21:19:31 +0900

    fix : 반환값 수정

--- src/diol/DBManager.h
+++ src/diol/DBManager.h
@@ -24,13 +24,14 @@ public:
 
     bool insert(uint64_t key, int value);
     int readData(uint64_t key);
-    map<unsigned int, int> range(uint64_t start, uint64_t end);
+    map<uint64_t, int> range(uint64_t start, uint64_t end);
 //    bool isFull(IMemtable& memtable);
     IMemtable* transformM0ToM1(IMemtable* memtable); // normal? delay?
 private:
     DBManager(): activeMemtableController(ActiveMemtableController::getInstance()),
                  immMemtableController(ImmutableMemtableController::getInstance()){
         currentId = 0;
+        getIdAndIncrement();
         compactionController->start();
         flushController->start();
     }
