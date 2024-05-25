da19a63f062dd5466361f8cd136b93ac1f29b684
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-05-25 21:24:35 +0900

    refactor(FlushController) : empty 체크

--- src/diol/FlushController.cpp
+++ src/diol/FlushController.cpp
@@ -2,20 +2,27 @@
 //#include "stddef.h"
 
 void FlushController::checkTimeout() {
+    cout<<"FlushController::checkTimeout()\n";
     ImmutableMemtableController& imm = ImmutableMemtableController::getInstance();
     bool timeoutFound = false;
-    for (const auto memtable : imm.normalImmMemtableList_M2) {
-        if (memtable->ttl == 0) {
-            imm.flushQueue.push(memtable);
-            imm.erase(imm.normalImmMemtableList_M2, memtable);
-            timeoutFound = true;
+    if(!imm.normalImmMemtableList_M2.empty()) {
+        for (const auto memtable: imm.normalImmMemtableList_M2) {
+            if (memtable->ttl <= 0) {
+                cout<<"flushQueue.push\n";
+                imm.flushQueue.push(memtable);
+                imm.erase(imm.normalImmMemtableList_M2, memtable);
+                timeoutFound = true;
+            }
         }
     }
-    for (const auto memtable : imm.delayImmMemtableList_M2) {
-        if (memtable->ttl == 0) {
-            imm.flushQueue.push(memtable);
-            imm.erase(imm.delayImmMemtableList_M2, memtable);
-            timeoutFound = true;
+    if(!imm.delayImmMemtableList_M2.empty()) {
+        for (const auto memtable: imm.delayImmMemtableList_M2) {
+            if (memtable->ttl <= 0) {
+                cout<<"flushQueue.push\n";
+                imm.flushQueue.push(memtable);
+                imm.erase(imm.delayImmMemtableList_M2, memtable);
+                timeoutFound = true;
+            }
         }
     }
     if (!timeoutFound) {
@@ -23,10 +30,12 @@ void FlushController::checkTimeout() {
         IMemtable* normalMem = findFlushMem(imm.normalImmMemtableList_M2);
         IMemtable* delayMem = findFlushMem(imm.delayImmMemtableList_M2);
         if(normalMem != NULL) {
+            cout<<"flushQueue.push\n";
             imm.flushQueue.push(normalMem);
             imm.erase(imm.normalImmMemtableList_M2, normalMem);
         }
         if(delayMem != NULL) {
+            cout<<"flushQueue.push\n";
             imm.flushQueue.push(delayMem);
             imm.erase(imm.delayImmMemtableList_M2, normalMem);
         }
@@ -36,12 +45,13 @@ void FlushController::checkTimeout() {
 // access count가 가장 적은 memtable find
 IMemtable* FlushController::findFlushMem(vector<IMemtable*>& v) {
     int minAccess = INT_MAX;
-    IMemtable* imm;
-
-    for (const auto memtable : v) {
-        if (memtable->access < minAccess) {
-            minAccess = memtable->access;
-            imm = memtable;
+    IMemtable* imm = NULL;
+    if(!v.empty()) {
+        for (const auto memtable: v) {
+            if (memtable->access < minAccess) {
+                minAccess = memtable->access;
+                imm = memtable;
+            }
         }
     }
     return imm;
