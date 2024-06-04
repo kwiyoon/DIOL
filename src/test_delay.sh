c2417993d312e1f8a4274005bccbc6ced394979b
haenalee0721 <haenalee0721@gmail.com>
2024-06-04 01:42:00 -0400

    add shell file for test

---
+++ src/test_delay.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PROGRAM="./testIoTDB"  # 실행 파일 경로를 지정하세요
+
+size=100000000
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <value>"
+  echo "Allowed values: 0.05, 0.1, 0.2, 0.3"
+  exit 1
+fi
+
+VALUE=$1
+
+# data와 files 배열 설정
+case $VALUE in
+  0.05)
+    data=("data_c100000000_d0.05")
+    files=("workloadA_data_c100000000_d0.05" "workloadB_data_c100000000_d0.05" "workloadC_data_c100000000_d0.05" "workloadD_data_c100000000_d0.05" "workloadE_data_c100000000_d0.05" "workloadF_data_c100000000_d0.05" "workloadG_data_c100000000_d0.05")
+    ;;
+  0.1)
+    data=("data_c100000000_d0.10")
+    files=("workloadA_data_c100000000_d0.10" "workloadB_data_c100000000_d0.10" "workloadC_data_c100000000_d0.10" "workloadD_data_c100000000_d0.10" "workloadE_data_c100000000_d0.10" "workloadF_data_c100000000_d0.10" "workloadG_data_c100000000_d0.10")
+    ;;
+  0.2)
+    data=("data_c100000000_d0.20")
+    files=("workloadA_data_c100000000_d0.20" "workloadB_data_c100000000_d0.20" "workloadC_data_c100000000_d0.20" "workloadD_data_c100000000_d0.20" "workloadE_data_c100000000_d0.20" "workloadF_data_c100000000_d0.20" "workloadG_data_c100000000_d0.20")
+    ;;
+  0.3)
+    data=("data_c100000000_d0.30")
+    files=("workloadA_data_c100000000_d0.30" "workloadB_data_c100000000_d0.30" "workloadC_data_c100000000_d0.30" "workloadD_data_c100000000_d0.30" "workloadE_data_c100000000_d0.30" "workloadF_data_c100000000_d0.30" "workloadG_data_c100000000_d0.30")
+    ;;
+  *)
+    echo "Invalid value: $VALUE"
+    echo "Allowed values: 0.05, 0.1, 0.2, 0.3"
+    exit 1
+    ;;
+esac
+
+# 배열 내용 출력
+for file in "${files[@]}"; do
+    echo "[ IoRDB ] Running with datas=${data} and workload=${file}"
+    $PROGRAM $size $data $file
+done
---
+++ src/test_workload.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+PROGRAM="./testIoTDB"  # 실행 파일 경로를 지정하세요
+
+size=100000000
+datas=("data_c100000000_d0.05" "data_c100000000_d0.10" "data_c100000000_d0.20" "data_c100000000_d0.30")
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <value>"
+  echo "Allowed values: A B C D E F G"
+  exit 1
+fi
+
+VALUE=$1
+
+# data와 files 배열 설정
+case $VALUE in
+  A)
+    files=("workloadA_data_c100000000_d0.05" "workloadA_data_c100000000_d0.10" "workloadA_data_c100000000_d0.20" "workloadA_data_c100000000_d0.30")
+    ;;
+  B)
+    files=("workloadB_data_c100000000_d0.05" "workloadB_data_c100000000_d0.10" "workloadB_data_c100000000_d0.20" "workloadB_data_c100000000_d0.30")
+    ;;
+  C)
+    files=("workloadC_data_c100000000_d0.05" "workloadC_data_c100000000_d0.10" "workloadC_data_c100000000_d0.20" "workloadC_data_c100000000_d0.30")
+    ;;
+  D)
+    files=("workloadD_data_c100000000_d0.05" "workloadD_data_c100000000_d0.10" "workloadD_data_c100000000_d0.20" "workloadD_data_c100000000_d0.30")
+    ;;
+  E)
+    files=("workloadE_data_c100000000_d0.05" "workloadE_data_c100000000_d0.10" "workloadE_data_c100000000_d0.20" "workloadE_data_c100000000_d0.30")
+    ;;
+  F)
+    files=("workloadF_data_c100000000_d0.05" "workloadF_data_c100000000_d0.10" "workloadF_data_c100000000_d0.20" "workloadF_data_c100000000_d0.30")
+    ;;
+  G)
+    files=("workloadG_data_c100000000_d0.05" "workloadG_data_c100000000_d0.10" "workloadG_data_c100000000_d0.20" "workloadG_data_c100000000_d0.30")
+    ;;
+  *)
+    echo "Invalid value: $VALUE"
+    echo "Allowed values: A, B, C, D, E, F, G"
+    exit 1
+    ;;
+esac
+
+# datas와 files에서 하나씩 선택하여 실행
+for i in "${!datas[@]}"; do
+    data=${datas[$i]}
+    file=${files[$i]}
+    echo "[ IoRDB ] Running with data=${data} and workload=${file}"
+    $PROGRAM $size $data $file
+done
